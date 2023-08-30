deeb894cebfd107c3b9acda6adbb664cb4e2a053
Daniel Augusto <daniel.augusto.milanez@gmail.com>
2023-08-30 16:58:51 -0300

    Primeira commit

---
+++ ConexaoAD/Conect.cs
@@ -0,0 +1,24 @@
+﻿using System.DirectoryServices;
+
+namespace ConexaoAD {
+    public class Conect {
+        public static bool AuthenticateUser(string NomeDeDominio, string NomeDeUsuario, string Senha) {
+            bool ret = false;
+
+            try {
+                DirectoryEntry diretorio = new DirectoryEntry("LDAP://" + NomeDeDominio, NomeDeUsuario, Senha);
+                DirectorySearcher dsearch = new DirectorySearcher(diretorio);
+                SearchResult results = null;
+
+                results = dsearch.FindOne();
+
+                ret = true;
+            }
+            catch {
+                ret = false;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ ConexaoAD/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ConexaoAD {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Insira seu nome de usuário: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string user = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Insira sua senha: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string password = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+            bool verificando = Conect.AuthenticateUser("192.168.1.12",user, password);
+
+            while (verificando == false) {
+                Console.WriteLine();
+                Console.WriteLine("Credenciais inválidas");
+
+                Console.Write("Insira seu nome de usuário: ");
+                user = Console.ReadLine();
+                Console.Write("Insira sua senha: ");
+                password = Console.ReadLine();
+
+                verificando = Conect.AuthenticateUser("192.168.1.12", user, password);
+            }
+            if (verificando == true) {
+                Console.WriteLine();
+                Console.Write($"Bem-Vindo ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(user);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
